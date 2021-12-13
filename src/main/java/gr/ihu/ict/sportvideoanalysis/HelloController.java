f18ed0842e501f9a102ead65a42eff83e2bd2ddf
SpGianniler <sp.gianniler@gmail.com>
2021-12-13 13:30:51 +0200

    first concept of the video analysis screen

--- src/main/java/gr/ihu/ict/sportvideoanalysis/HelloController.java
+++ src/main/java/gr/ihu/ict/sportvideoanalysis/HelloController.java
@@ -1,14 +1,23 @@
 package gr.ihu.ict.sportvideoanalysis;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+//        nothing to initialize yet
     }
 }
---
+++ src/main/java/gr/ihu/ict/sportvideoanalysis/Main.java
@@ -0,0 +1,40 @@
+package gr.ihu.ict.sportvideoanalysis;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("mainScreen-view.fxml")));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        Scene scene = new Scene(root);
+
+        root.setOnMousePressed(event->{
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - xOffset);
+            primaryStage.setY(event.getScreenY() - yOffset);
+        });
+
+        primaryStage.setScene(scene);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
